454f33c421a7a15e3118c86209130beb96b54e67
u110 <web.u.project@gmail.com>
2020-01-23 23:22:15 +0900

    fix

--- cmd/main.go
+++ cmd/main.go
@@ -13,7 +13,22 @@ func ReGenerateRoom(arr []*area.Area) {
 	}
 }
 
+func test() {
+	rand.Seed(3)
+	fmt.Println("start")
+	x, y := 100, 50
+	m := area.Area{Id: 0, TL: []int{0, 0}, BR: []int{x, y}}
+	m.SepH()
+	m.GenRoom()
+	m.GenPath()
+	m.Child.GenRoom()
+	m.Child.GenPath()
+	m.LinkPath()
+	m.ShowRange(x, y)
+}
+
 func main() {
+
 	rand.Seed(time.Now().UnixNano()) // set random seed
 	x, y := 100, 50
 
@@ -37,9 +52,13 @@ func main() {
 			fmt.Println(target.NextTo)
 			time.Sleep(500 * time.Millisecond)
 
+			target.Paths = append(target.Paths, target.Child.Paths...)
 			target = target.Child
 			areaArr = append(areaArr, target)
 			countup++
+		}
+		for _, a := range areaArr {
+			a.LinkPath()
 		}
 		mainArea.ShowRange(x, y)
 		time.Sleep(500 * time.Millisecond)
